2d0147027a843120689342a80c9870f72cd5a2cc
Derrick Myers <derrmyer@udel.edu>
2026-02-11 12:58:42 -0500

    added image and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import "./App.css";
+import catPhoto from "./spexels-peter-xie-371876898-34574274.jpg";
 
 function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript <br />
+                <h1>UD CISC275 with React Hooks and TypeScript</h1> <br />
                 Hello World
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Derrick was here!
             </p>
+            <img src={catPhoto} alt="free use cute cat photo" />
         </div>
     );
 }
