d8e29e93cee2f8e9b796c8ad0ef8bc061d256949
Derrick Myers <derrmyer@udel.edu>
2026-02-16 11:59:58 -0500

    task_3_complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./App.css";
-import catPhoto from "./spexels-peter-xie-371876898-34574274.jpg";
+import catPhoto from "./pexels-peter-xie-371876898-34574274.jpg";
+import { Button } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -8,12 +12,44 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 <h1>UD CISC275 with React Hooks and TypeScript</h1> <br />
                 Hello World
+                <p>Derrick was here!</p>
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Derrick was here!
-            </p>
-            <img src={catPhoto} alt="free use cute cat photo" />
+            <img
+                src={catPhoto}
+                alt="free use cute cat photo"
+                width="500"
+                height="300"
+            />
+            <p>Shopping List</p>
+            <ul>
+                <li>Bread</li>
+                <li>Cheese</li>
+                <li>Milk</li>
+            </ul>
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            className="rectangle"
+                            style={{ backgroundColor: "red" }}
+                        />
+                    </Col>
+                    <Col>
+                        <div
+                            className="rectangle"
+                            style={{ backgroundColor: "red" }}
+                        />
+                    </Col>
+                    q
+                </Row>
+            </Container>
         </div>
     );
 }
