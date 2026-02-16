5a8264585f56298999183c6aa3444085a866a88f
Derrick Myers <derrmyer@udel.edu>
2026-02-16 12:50:06 -0500

    task_4_complete

--- src/functions.ts
+++ src/functions.ts
@@ -43,5 +43,7 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    if (word.toLowerCase() === "yes") return true;
+    if (word.toLowerCase() === "no") return false;
+    return null;
 }
