5bb464998b93a65e518ab2ee6de097e88d43cae8
Derrick Myers <derrmyer@udel.edu>
2026-02-23 14:06:27 -0500

    allRGB Complete

--- src/arrays.ts
+++ src/arrays.ts
@@ -69,7 +69,9 @@ export function countShortWords(words: string[]): number {
  * then return true.
  */
 export function allRGB(colors: string[]): boolean {
-    return false;
+    return colors.every((color: string): boolean =>
+        ["red", "green", "blue"].includes(color),
+    );
 }
 
 /**
