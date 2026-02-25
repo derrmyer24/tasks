079c7400085cd606a8934981f69e5a2f43973587
Derrick Myers <derrmyer@udel.edu>
2026-02-25 11:58:56 -0500

    changQuestionTypeById Complete

--- src/nested.ts
+++ src/nested.ts
@@ -189,9 +189,23 @@ export function changeQuestionTypeById(
     targetId: number,
     newQuestionType: QuestionType,
 ): Question[] {
-    return [];
+    return questions.map(
+        (question: Question): Question =>
+            question.id === targetId ?
+                {
+                    ...question,
+                    type: newQuestionType,
+                    options:
+                        (
+                            question.type === "multiple_choice_question" &&
+                            newQuestionType !== "multiple_choice_question"
+                        ) ?
+                            []
+                        :   question.options,
+                }
+            :   question,
+    );
 }
-
 /**
  * Consumes an array of Questions and produces a new array of Questions, where all
  * the Questions are the same EXCEPT for the one with the given `targetId`. That
