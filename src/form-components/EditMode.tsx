bc32fc31dfb9877a8cf2114d4501d54284c95054
Derrick Myers <derrmyer@udel.edu>
2026-03-18 16:00:30 -0400

    editMode complete

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,54 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): React.JSX.Element {
+    const [isEditMode, setEditMode] = useState<boolean>(false);
+    const [userName, setUserName] = useState<string>("Your Name");
+    const [isStudent, setIsStudent] = useState<boolean>(true);
+
+    function updateEditMode(event: React.ChangeEvent<HTMLInputElement>) {
+        setEditMode(event.target.checked);
+    }
+
+    function updateIsStudent(event: React.ChangeEvent<HTMLInputElement>) {
+        setIsStudent(event.target.checked);
+    }
+
+    function updateuserName(event: React.ChangeEvent<HTMLInputElement>) {
+        setUserName(event.target.value);
+    }
     return (
         <div>
-            <h3>Edit Mode</h3>
+            <h3>Change Color</h3>
+            <Form.Check
+                type="switch"
+                id="is-editMode-check"
+                label="Edit?"
+                checked={isEditMode}
+                onChange={updateEditMode}
+            />
+            {!isEditMode &&
+                (isStudent ?
+                    <div>{userName} is a student.</div>
+                :   <div>{userName} is not a student.</div>)}
+            {isEditMode && (
+                <div>
+                    <Form.Check
+                        type="checkbox"
+                        id="is-student-check"
+                        label="Student?"
+                        checked={isStudent}
+                        onChange={updateIsStudent}
+                    />
+                    <Form.Group controlId="userName-textbox">
+                        <Form.Label>Name:</Form.Label>
+                        <Form.Control
+                            value={userName}
+                            onChange={updateuserName}
+                        />
+                    </Form.Group>
+                </div>
+            )}
         </div>
     );
 }
